d706898a0bb65fc2d925f202e7c32f48869bfb9a
saerom-hong <saeromhong12@gmail.com>
2022-02-15 13:55:42 +0100

    build Project #1 - add class

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,69 @@
-console.log("Learning!");
+//autobind decorator instead of bind(this) method
+function autobind(
+  target: any,
+  methodName: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    //render content
+    const importedHtmlContent = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedHtmlContent.firstElementChild as HTMLFormElement;
+    //add css styling
+    this.element.id = "user-input";
+    //access input value
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+    this.configure();
+    this.attach();
+  }
+  @autobind
+  private submitHandler(e: Event) {
+    //prevent HTP request to be sent
+    e.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler);
+  }
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+//instantiate
+const Input1 = new ProjectInput();
